bf788c511a133c53fc6c330bf3a6bdb54791238a
22000023 <22000023@mvula.univen.ac.za>
2024-08-03 03:31:01 +0200

    updated the index file

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,7 @@
-function navbarAction() {
+function sidebarAction() {
     const sidebar = document.querySelector('.sidebar');
     const container = document.querySelector('.container');
 
-    if (sidebar.classList.contains('hidden')) {
-        // Show sidebar and adjust container
-        sidebar.classList.remove('hidden');
-        container.classList.remove('active');
-    } else {
-        // Hide sidebar and adjust container
-        sidebar.classList.add('hidden');
-        container.classList.add('active');
-    }
+    sidebar.classList.toggle('open');
+    container.classList.toggle('expand');
 }
