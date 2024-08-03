ded7dad36176819f7dfb992949877a7cc1b85cf7
22000023 <22000023@mvula.univen.ac.za>
2024-08-03 15:09:48 +0200

    good update

--- scripts/index.js
+++
@@ -1,7 +0,0 @@
-function sidebarAction() {
-    const sidebar = document.querySelector('.sidebar');
-    const container = document.querySelector('.container');
-
-    sidebar.classList.toggle('open');
-    container.classList.toggle('expand');
-}
